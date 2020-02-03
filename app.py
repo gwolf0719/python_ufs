d3fff1298012780f7470be900faaa8e330410536
James <james@fofo.tw>
2020-02-03 15:03:39 +0800

    try except

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import time
 import numpy as np
 
 app = Flask(__name__)
+client = pymongo.MongoClient("mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+
 
 @app.route("/")
 def hello():
@@ -14,15 +16,16 @@ def hello():
 
 @app.route("/webhook", methods=["POST","GET"])
 def webhook():
-
-    client = pymongo.MongoClient(
-        "mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
     
-    jsondata = request.get_json()
+    try:
+        jsondata = request.get_json()
+    except:
+        jsondata = {'data': 'nodata'}
+    
+    
     mycol = client.ufs.webhook
     
     df = pd.DataFrame(jsondata, index=[0])
     mycol.insert_many(df.to_dict('records'))
-
 
     return "200"
