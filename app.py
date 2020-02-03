a7c77c9c9a3bbc092e0fa6a175d9c4ff4d18bf21
James <james@fofo.tw>
2020-02-03 14:41:19 +0800

    api webhook 來函照登

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-from pymongo import MongoClient
-
+from flask import Flask, jsonify, request
+import json
+import pymongo
+import pandas as pd
+import datetime
+import time
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,4 +14,15 @@ def hello():
 
 @app.route("/webhook", methods=["POST","GET"])
 def webhook():
+
+    client = pymongo.MongoClient(
+        "mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    
+    jsondata = request.get_json()
+    mycol = client.ufs.webhook
+    
+    df = pd.DataFrame(jsondata, index=[0])
+    mycol.insert_many(df.to_dict('records'))
+
+
     return "webhook"
