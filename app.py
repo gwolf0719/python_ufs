0a0853a15f328604b0cd41423ad1b28c69430be3
James <james@fofo.tw>
2020-02-03 16:40:34 +0800

    userId

--- app.py
+++ app.py
@@ -27,15 +27,16 @@ def webhook(channel_id):
     jsondata = request.get_json()
     print(jsondata)
     try:
-        
         jsondata["channel_id"] = channel_id
-
-        replyToken = jsondata["events"][0]["replyToken"]
-        # print(replyToken)
-        # replyToken = "41efe2333b5049559a7400d855483e5f"
+        event = jsondata["events"][0]
+        replyToken = event["replyToken"]
+        userId = event["source"]["userId"]
         line_bot_api = LineBotApi(
             'EeW1IZR3U3fYS9rVH1njiVkTlaRUFEvkyXS2xl1swT+p+McTNzdZwZphg1BrjvjTXXcQAlSHK/I2bx2s3Fu8GfUS5tljY2ZO8krNSKgpU6O7GRgwMcxKHfQvp7w4m8PHZZmsGy9C3pf4ifaXws7/+wdB04t89/1O/w1cDnyilFU=')
         line_bot_api.reply_message(replyToken, TextSendMessage(text='Hello World!'))
+        line_bot_api.push_message(
+            userId, TextSendMessage(text='Hello World! userId'))
+
 
     except:
         jsondata = {'data': 'nodata'}
