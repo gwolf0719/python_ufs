ba23ae45c3ad951f39ba4c7e052175d5868a184b
James <james@fofo.tw>
2020-02-10 12:18:38 +0800

    初始環境＋管理者登入登出

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template,session,redirect,url_for
+import os
 import json
 import pymongo
 import pandas as pd
 import datetime
+from datetime import timedelta
 import time
 import numpy as np
 
@@ -11,29 +13,55 @@ from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
 
+from data_model.manager import *
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=31)
 client = pymongo.MongoClient("mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 
+manager = Manager()
 
-@app.route("/")
-def hello():
-    return "Hello, World! 3 "
-
-
+@app.route("/login",methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        manager_id = request.values["manager_id"]
+        manager_pwd = request.values["manager_pwd"]
+        
+        
+        # 先確認目前登入狀態
+        if(manager.chk_now() == True):
+            return "login done"+session.get("manager_id")
+        else:
+            # 如果未登入就去驗證
+            if(manager.chk_id_pw(manager_id,manager_pwd) == True):
+                manager.login(manager_id)
+                return "login doing"+session.get("manager_id")
+            else:
+                return "login failed"
+    else:
+        return render_template('login.html')
+@app.route('/logout', methods=['GET'])
+def logout():
+    
+    manager.logout()
+    return redirect(url_for("login"))
 
 # 單純抓取 webhook 回傳資料
 @app.route("/webhook/<channel_id>", methods=["POST", "GET"])
 def webhook(channel_id):
     jsondata = request.get_json()
-    print(jsondata)
     try:
         jsondata["channel_id"] = channel_id
         event = jsondata["events"][0]
         replyToken = event["replyToken"]
-        userId = event["source"]["userId"]
+        # 回覆
         line_bot_api = LineBotApi(
             'EeW1IZR3U3fYS9rVH1njiVkTlaRUFEvkyXS2xl1swT+p+McTNzdZwZphg1BrjvjTXXcQAlSHK/I2bx2s3Fu8GfUS5tljY2ZO8krNSKgpU6O7GRgwMcxKHfQvp7w4m8PHZZmsGy9C3pf4ifaXws7/+wdB04t89/1O/w1cDnyilFU=')
         line_bot_api.reply_message(replyToken, TextSendMessage(text='Hello World!'))
+        # 主動發送
+        userId = event["source"]["userId"]
         line_bot_api.push_message(
             userId, TextSendMessage(text='Hello World! userId'))
 
@@ -41,7 +69,13 @@ def webhook(channel_id):
     except:
         jsondata = {'data': 'nodata'}
         replyToken = 'null'
+        
     mycol = client.ufs.webhook
     df = pd.DataFrame(jsondata, index=[0])
     mycol.insert_many(df.to_dict('records'))
     return "replyToken"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ data_model/manager.py
@@ -0,0 +1,51 @@
+#coding=utf-8
+from flask import Flask, jsonify, request, render_template,session
+import pymongo
+import pandas as pd
+import datetime
+import time
+import numpy as np
+
+class Manager:
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    # 確認目前登入狀態
+    def chk_now(self):
+        if session.get("manager_id") is None:
+            return False
+        else:
+            return True
+    # 取得單一帳號資料
+    def get_once(self,manager_id):
+        manager = self.client.ufs.manager
+        manager_data = manager.find_one({"manager_id":manager_id})
+        self.manager_data = manager_data
+        return manager_data
+    # 驗證帳密正確性
+    def chk_id_pw(self,manager_id,manager_pwd):
+        manager_data = Manager.get_once(self,manager_id)
+        if(manager_data["manager_pwd"] == manager_pwd):
+            return True
+        else:
+            return False
+    
+
+    # 登入
+    def login(self,manager_id):
+        session["manager_id"] = manager_id
+    # 登出
+    def logout(self):
+        session['manager_id'] = False
+        
+
+
+    # 宣告資料庫和資料表
+        # manager = self.client.thermometer.thermometer_mac
+        # cursor = mycol.find({'mac_id': mac_id})
+        # count_result = cursor.count()
+        # if(count_result == 0):
+        #     return False
+        # else:
+        #     return True
+
+    # 確認帳號密碼正確
