6e033ecd93d312a3725c1024da2e7ddabbd4d563
James <james@fofo.tw>
2020-02-10 17:35:00 +0800

    index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template,session,redirect,url_for
+from flask import Flask, jsonify, request, render_template,session,redirect,url_for,flash
 import os
 import json
 import pymongo
@@ -19,61 +19,71 @@ from data_model.manager import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=31)
-client = pymongo.MongoClient("mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+# client = pymongo.MongoClient("mongodb://james:wolf0719@cluster0-shard-00-01-oiynz.azure.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 manager = Manager()
-
+# 登入管理者
 @app.route("/login",methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
         manager_id = request.values["manager_id"]
         manager_pwd = request.values["manager_pwd"]
-        
-        
         # 先確認目前登入狀態
         if(manager.chk_now() == True):
-            return "login done"+session.get("manager_id")
+            return redirect(url_for("index"))
         else:
             # 如果未登入就去驗證
             if(manager.chk_id_pw(manager_id,manager_pwd) == True):
+                # 登入 manager_id
                 manager.login(manager_id)
-                return "login doing"+session.get("manager_id")
+                return redirect(url_for("index"))
             else:
-                return "login failed"
+                flash(manager_id +" 帳號登入失敗，請檢查登入訊息後再重新登入")
+                return render_template('login.html')
     else:
         return render_template('login.html')
+# 登出
 @app.route('/logout', methods=['GET'])
 def logout():
-    
     manager.logout()
     return redirect(url_for("login"))
 
+
+@app.route("/", methods=["GET"])
+@app.route("/channel", methods=["GET"])
+def index():
+    if(manager.chk_now() == True):
+        return render_template("index.html")
+    else:
+        return redirect(url_for("login"))
+
+
 # 單純抓取 webhook 回傳資料
-@app.route("/webhook/<channel_id>", methods=["POST", "GET"])
-def webhook(channel_id):
-    jsondata = request.get_json()
-    try:
-        jsondata["channel_id"] = channel_id
-        event = jsondata["events"][0]
-        replyToken = event["replyToken"]
-        # 回覆
-        line_bot_api = LineBotApi(
-            'EeW1IZR3U3fYS9rVH1njiVkTlaRUFEvkyXS2xl1swT+p+McTNzdZwZphg1BrjvjTXXcQAlSHK/I2bx2s3Fu8GfUS5tljY2ZO8krNSKgpU6O7GRgwMcxKHfQvp7w4m8PHZZmsGy9C3pf4ifaXws7/+wdB04t89/1O/w1cDnyilFU=')
-        line_bot_api.reply_message(replyToken, TextSendMessage(text='Hello World!'))
-        # 主動發送
-        userId = event["source"]["userId"]
-        line_bot_api.push_message(
-            userId, TextSendMessage(text='Hello World! userId'))
+# @app.route("/webhook/<channel_id>", methods=["POST", "GET"])
+# def webhook(channel_id):
+#     jsondata = request.get_json()
+#     try:
+#         jsondata["channel_id"] = channel_id
+#         event = jsondata["events"][0]
+#         replyToken = event["replyToken"]
+#         # 回覆
+#         line_bot_api = LineBotApi(
+#             'EeW1IZR3U3fYS9rVH1njiVkTlaRUFEvkyXS2xl1swT+p+McTNzdZwZphg1BrjvjTXXcQAlSHK/I2bx2s3Fu8GfUS5tljY2ZO8krNSKgpU6O7GRgwMcxKHfQvp7w4m8PHZZmsGy9C3pf4ifaXws7/+wdB04t89/1O/w1cDnyilFU=')
+#         line_bot_api.reply_message(replyToken, TextSendMessage(text='Hello World!'))
+#         # 主動發送
+#         userId = event["source"]["userId"]
+#         line_bot_api.push_message(
+#             userId, TextSendMessage(text='Hello World! userId'))
 
 
-    except:
-        jsondata = {'data': 'nodata'}
-        replyToken = 'null'
+#     except:
+#         jsondata = {'data': 'nodata'}
+#         replyToken = 'null'
         
-    mycol = client.ufs.webhook
-    df = pd.DataFrame(jsondata, index=[0])
-    mycol.insert_many(df.to_dict('records'))
-    return "replyToken"
+#     mycol = client.ufs.webhook
+#     df = pd.DataFrame(jsondata, index=[0])
+#     mycol.insert_many(df.to_dict('records'))
+#     return "replyToken"
 
 
 if __name__ == '__main__':
