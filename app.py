1757a10725867a8bdcf38268a5deadcd1e10bd5c
Effy <miyabilee@gmail.com>
2020-03-02 13:27:03 +0800

    商品管理靜態

--- app.py
+++ app.py
@@ -200,8 +200,19 @@ def tags():
 
 
 
-
-
+# 商品管理
+@app.route("/products", methods=["GET", "POST"])
+def products():
+    if(manager.chk_now() == True):
+        manager_id = session.get("manager_id")
+        if session.get("channel_id") is None:
+            flash("請先選取要設定的 Channel ","danger")
+            return redirect(url_for("channel"))
+        else:
+            channel_id = session.get("channel_id")
+            return render_template("products.html")
+    else:
+        return redirect(url_for("login"))
 
 
 
