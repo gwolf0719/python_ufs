33472508697b71474c3f8f6a2d5bd8970243d780
James <james@fofo.tw>
2020-03-24 16:43:42 +0800

    app.config['SECRET_KEY'] = 'jameswolf'

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ from api_sys import *
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'redis'  # session类型为redis
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SECRET_KEY'] = 'jameswolf'
 app.config['PERMANENT_SESSION_LIFETIME'] = 7200
 app.config['SESSION_REDIS'] = redis.Redis(host='127.0.0.1', port='6379', db=4) 
 app.register_blueprint(api)
