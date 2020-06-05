97e29faa0991951ed843f5f8d0326d9da0a83383
James <james@fofo.tw>
2020-06-05 17:07:48 +0800

    debug

--- app.py
+++ app.py
@@ -474,7 +474,9 @@ def webhook(channel_id):
             
             # 開始追縱歡迎
             if event['type'] == 'follow':
+                print('follow');
                 if 'welcome' in channel_data:
+                    print('welcome');
                     rebot_text = channel_data['welcome']
                     line_bot_api.reply_message(replyToken, TextSendMessage(text=rebot_text))
                     chat_data['text'] = event['message']['text']
